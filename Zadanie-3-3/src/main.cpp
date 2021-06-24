f4c93a09cfee39db3bfb389697c10b43444f7209
kamilmarcinow <kamil.marcinowski@student.wat.edu.pl>
2021-06-25 00:37:04 +0200

    "Wykonano zadanie 3.3.1 – tekst statyczny”

--- Zadanie-3-3/src/main.cpp
+++ Zadanie-3-3/src/main.cpp
@@ -1,9 +1,13 @@
 #include <Arduino.h>
+#include <LiquidCrystal.h>
+#include <Wire.h>
 
+LiquidCrystal lcd (12, 11, 5, 4, 3, 2);
 void setup() {
-  // put your setup code here, to run once:
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+lcd.begin(16, 2);
+  lcd.print("Marcinowski");}
+  void loop() {
+lcd.setCursor(0, 1);
+    lcd.print(millis() / 1000);
+    delay(500);
+  }
